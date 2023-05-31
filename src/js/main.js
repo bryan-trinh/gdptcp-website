2d62d9ac6551c6bec2f89bd78baff9f2aa9be0fa
Bryan Trinh <trinhbd@uci.edu>
2023-05-30 19:09:09 -0700

    added light/dark mode, dynamic main banner scaling, sample test button function, lotus emblem

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,72 @@
-import * as bootstrap from 'bootstrap'
+// CSS Styles
+import "../scss/styles.scss";
 
-import React from "react"
-import ReactDOM from "react-dom"
+// React
+import ReactDOM from "react-dom";
+import { createContext, useState } from "react";
+import ReactSwitch from "react-switch";
 
-ReactDOM.render(<h1>With React</h1>, document.getElementById("root"))
+import logo from "../assets/lotus.png";
+
+/* Main App
+ * Developed by Bryan Trinh
+ */
+export const ThemeContext = createContext(null);
+
+const App = () => {
+    const [theme, setTheme] = useState("light");
+
+    const toggleTheme = () =>{
+        setTheme((curr) => (curr === "light" ? "dark" : "light"));
+    }
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme}}>
+            <div className="App" id={theme}>
+
+                <div class="container-fluid py-4 mx-auto">
+                    <div className="switch">
+                        <label> {theme === "light" ? "Light Mode" : "Dark Mode"}</label>
+                        <ReactSwitch onChange={toggleTheme} checked={theme === "dark"}/>
+                    </div>
+                    
+                    <div className="main-banner-text">
+                        <div>
+                            <img src={require("../assets/lotus.png")}/>
+                            <h1>Gia Đình Phật Tử Chánh Pháp</h1>
+                        </div>
+                        <subtitle>Built using Bootstrap, Parcel, and React</subtitle>
+                    </div>
+                    <button id="testBtn" class="btn btn-outline-primary">TestBtn</button>
+                </div>
+            </div>
+        </ThemeContext.Provider>
+
+        
+    )
+}
+ReactDOM.render(<App />, document.getElementById("root"))
+
+document.getElementById("testBtn").addEventListener("click", testFunction);
+function testFunction(event){
+    const btn = event.target;
+    alert("The button will be disabled for 5 seconds");
+    btn.setAttribute("disabled", "");
+    
+    countDown(btn, btn.innerText);
+}
+var count = 5;
+function countDown(obj, text){
+    if(count !== 0){
+        obj.innerText = count;
+        setTimeout(countDown.bind(this, obj, text), 1000);
+        count--;
+    }
+    else{
+        obj.removeAttribute("disabled");
+        count = 5;
+        obj.innerText = text;
+    }
+}
+
+export default App;
